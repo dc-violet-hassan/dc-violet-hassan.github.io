72cad87bf639ace84edbda3c3d709cdf6a67b253
dc-violet-hassan <violet.hassan@dcmail.ca>
2025-11-08 13:33:20 -0500

    Evil circle is now functional

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -83,8 +83,9 @@ class Ball extends Shape {
 class EvilCircle extends Shape {
   constructor(x, y){
     super(x, y, 20, 20);
-    this.color = "rgb(0 0 0)";
+    this.color = "rgb(255, 255, 255)";
     this.size = 10;
+    
     window.addEventListener("keydown", (e) => {
       switch (e.key) {
         case "a":
@@ -101,6 +102,46 @@ class EvilCircle extends Shape {
           break;
       }
     });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x -= this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x += this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y -= this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y += this.size;
+    }
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+        }
+      }
+    }
   }
 }
 
@@ -125,14 +166,22 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+player = new EvilCircle(500, 500);
+
 function loop() {
   ctx.fillStyle = "rgb(0 0 0 / 25%)";
   ctx.fillRect(0, 0, width, height);
 
+  player.draw();
+  player.checkBounds();
+  player.collisionDetect();
+
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
   }
 
   requestAnimationFrame(loop);
