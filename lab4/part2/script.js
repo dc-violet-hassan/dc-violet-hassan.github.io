d86798351cc132fd2023d6eb508b3ee3ab9e73f7
dc-violet-hassan <violet.hassan@dcmail.ca>
2025-11-08 11:42:16 -0500

    thumb bar images added and updateDisplayedImage function complete

--- lab4/part2/script.js
+++ lab4/part2/script.js
@@ -5,9 +5,30 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 const images = [
-    {filename: pic1.jpg, alt: "Closeup of a human eye"},
-    {filename: pic2.jpg, alt: "Rock that looks like a wave"},
-    {filename: pic3.jpg, alt: "Purple and white pansies"},
-    {filename: pic4.jpg, alt: "Section of wall from a pharaoh's tomb"},
-    {filename: pic5.jpg, alt: "Large moth on a leaf"}
-]
+    {filename: "pic1.jpg", alt: "Closeup of a human eye"},
+    {filename: "pic2.jpg", alt: "Rock that looks like a wave"},
+    {filename: "pic3.jpg", alt: "Purple and white pansies"},
+    {filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb"},
+    {filename: "pic5.jpg", alt: "Large moth on a leaf"}
+];
+
+const baseURL = "./images/";
+
+for(const image of images){
+    const newImage = document.createElement("img");
+    newImage.src = baseURL + image.filename;
+    newImage.alt = image.alt;
+    newImage.tabIndex = "0";
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", updateDisplayedImage);
+    newImage.addEventListener("keydown", (event) => {
+        if (event.code === "Enter") {
+            updateDisplayedImage();
+        }
+    });
+}
+
+function updateDisplayedImage(event){
+    displayedImage.src = event.target.src;
+    displayedImage.alt = event.target.alt;
+}
